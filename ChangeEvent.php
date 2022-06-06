ad7b48696bd1ff023de937a3254c2376e215ee6c
carlottesen <79794759+carlottesen@users.noreply.github.com>
2022-06-06 19:05:43 +0200

    tilføjet en flot knap

---
+++ ChangeEvent.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" href="style.css">
+<body>
+
+
+<?php
+
+include_once 'TopNav.html';
+include_once 'ShowUser.php';
+
+//Let the user change the event with the id present in the changeEvent cookie
+
+
+?>
+</body>
+</html>
--- ShowEvents.php
+++ ShowEvents.php
@@ -3,6 +3,24 @@
 <link rel="stylesheet" href="style.css">
 <body>
 
+<script>
+    function changeEvent(id) {
+        //Go to ChangeEvent.php and pass the id of the event as a cookie
+        var cookie_name = "changeEvent";
+        var cookie_value = id;
+        document.cookie = cookie_name + "=" + cookie_value + "; path=/";
+        window.location.replace("http://localhost/uiswebsite/ChangeEvent.php");
+    }
+</script>
+
+<?php
+function changeEventAux($id){
+    $cookie_name = "event2bChanged";
+	$cookie_value = $id;
+	setcookie($cookie_name, $cookie_value["SSN"], time() + (86400 * 30), "/"); // 86400 = 1 day
+}
+//php_func();
+?>
 
 <?php
 
@@ -42,19 +60,34 @@ $result = $conn->query(
     WHERE SSN='$cookie'");
 
 if ($result->num_rows > 0) {
-    echo "<table><tr><th>Name</th><th>BegivenhedNr</th><th>Emne</th><th>StartDato</th><th>SlutDato</th><th>Anmodet svar</th></tr>";
+    echo "<table><tr>
+    <th>Name</th>
+    <th>BegivenhedNr</th>
+    <th>Emne</th>
+    <th>StartDato</th>
+    <th>SlutDato</th>
+    <th>Anmodet svar</th>
+    <th>Ændrer Begivenhed</th></tr>";
     // output data of each row
     while($row = $result->fetch_assoc()) {
         $requstedAnswer = "Nej";
         if ($row["RequstedAnswer"] == 1) {
             $requstedAnswer = "Ja";
         }
-        echo "<tr><td>" . $row["Name"]. "</td><td>" . $row["EventID"]. "</td><td>" . $row["Subject"]. "</td><td>" . $row["StartTime"]. "</td><td>" . $row["EndTime"].
-        "</td><td>" . $requstedAnswer. "</td></tr>";
+        //Display the event name, id, subject, start, end time, if the user has requested to attend the event and a button for changing the event
+        echo "<tr>
+        <td>" . $row["Name"]. "</td>
+        <td>" . $row["EventID"]. "</td>
+        <td>" . $row["Subject"]. "</td>
+        <td>" . $row["StartTime"]. "</td>
+        <td>" . $row["EndTime"]. "</td>
+        <td>" . $requstedAnswer. "</td>
+        <td> . <button onclick=\"changeEvent(" . $row["EventID"] . ")\">Ændre Begivenhed</button> </td>
+        </tr>";
     }
     echo "</table>";
 } else {
-    echo "<h1 style='color: red;'>Du har ikke lavet nogen events</h1>";
+    echo "<h1 style='color: red;'>Du har ikke lavet nogle events</h1>";
 }
 
 
@@ -84,7 +117,7 @@ if ($result->num_rows > 0) {
     }
     echo "</table>";
 } else {
-    echo "<h1 style='color: red;'>Du er ikke blevet inviteret til nogen begivenheder</h1>";
+    echo "<h1 style='color: red;'>Du er ikke blevet inviteret til nogle begivenheder</h1>";
 }
 
 echo "</div>";
